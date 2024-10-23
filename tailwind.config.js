fdbc4faa9575f795e0142a9aff345ffafbe22c22
Mariana Kemmerich <marianarkemmerich@gmail.com>
2024-10-23 11:03:15 -0300

    chore: add tailwind css setup and initial configuration

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        skyBlue: '#78B0D0',
+        forestGreen: '#55723C',
+        leafGreen: '#6E9A47',
+        gold: '#CFB040',
+        sunshine: '#F0C117',
+      }
+    },
+  },
+  plugins: [],
+}
+
