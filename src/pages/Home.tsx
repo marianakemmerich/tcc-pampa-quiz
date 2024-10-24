5a0969b0fe5c5d1dfe82378476ab435232a77db8
Mariana Kemmerich <marianarkemmerich@gmail.com>
2024-10-24 01:52:16 -0300

    feat: integrate button component on home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import logo from '../assets/logo/pampa-quiz-logo.png'
+import Button from '../components/Button'
 
 const Home = () => {
   return (
-    <div className='bg-skyBlue w-screen h-screen flex items-center justify-center'>
-        <div className='flex items-center justify-center'>
-            <img src={logo} alt="Pampa Quiz logo" className='w-[3/5] md:w-[500px] mb-14' />
-        </div>
+    <div className='bg-skyBlue w-screen h-screen items-center justify-center flex flex-col'>
+      <div className='flex flex-col items-center justify-center'>
+        <img src={logo} alt="Pampa Quiz logo" className='w-[3/5] md:w-[500px]' />
+      </div>
+      <Button label='Jogar' route='/game' className='mt-14'/>
     </div>
   )
 }
