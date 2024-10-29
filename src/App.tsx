5f561c7a3bf2124b6b94a91138d472e71d8255ae
Mariana Rodrigues Kemmerich <marianarkemmerich@gmail.com>
2024-10-29 13:13:06 -0300

    feat: add route to menu page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Home from '../src/pages/Home'
+import Menu from '../src/pages/Menu'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/menu" element={<Menu />} />
       </Routes>
     </Router>
   )
