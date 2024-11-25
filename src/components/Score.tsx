26de9823051a0cf68ed491512d9ee56fe6c115c0
Mariana Rodrigues Kemmerich <marianarkemmerich@gmail.com>
2024-11-25 01:55:04 -0300

    feat: add score component

---
+++ src/components/Score.tsx
@@ -0,0 +1,16 @@
+// components/Score.tsx
+import React from 'react'
+
+interface ScoreProps {
+  points: number
+}
+
+const Score = ({ points }: ScoreProps) => {
+  return (
+    <div className="text-xl font-semibold text-white">
+      {points}
+    </div>
+  )
+}
+
+export default Score
