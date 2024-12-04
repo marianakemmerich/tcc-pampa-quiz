0bc1a5ee8dd057f15c5a584a95e18c36b2989bf2
Mariana Rodrigues Kemmerich <marianarkemmerich@gmail.com>
2024-12-04 12:39:08 -0300

    style: improve general style for category card component

--- src/components/CategoryCard.tsx
+++ src/components/CategoryCard.tsx
@@ -1,42 +1,16 @@
-import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import LevelModal from './LevelModal'
+import React from 'react'
+import { CategoryCardProps } from '../types/CategoryCard'
 
-interface CategoryCardProps {
-    label: string
-    category: string
-    image: string
-    className?: string
+const CategoryCard = ({ label, category, image, className, onClick }: CategoryCardProps) => {
+  return (
+    <div
+      onClick={onClick}
+      className={`card ${className} w-[300px] h-[200px] flex items-center p-4 gap-4 rounded-md cursor-pointer`}
+    >
+      <img src={image} alt={category} className="w-[150px] object-contain" />
+      <p className="text-4xl font-bold text-white">{label}</p>
+    </div>
+  )
 }
-
-const CategoryCard = ({ label, category, image, className }: CategoryCardProps) => {
-    const [isModalOpen, setModalOpen] = useState(false)
-    const [selectedLevel, setSelectedLevel] = useState('easy')
-    const navigate = useNavigate()
-
-    const handleStartGame = () => {
-        navigate(`/game?category=${category}&level=${selectedLevel}`);
-    };
-
-    return (
-        <div
-            className={`w-[300px] h-[150px] relative cursor-pointer rounded-lg p-4 flex items-center shadow-lg text-center ${className}`}
-            onClick={() => setModalOpen(true)}
-        >
-            <div className='flex items-center w-full'>
-                <img src={image} alt={label} className='w-[150px] object-cover rounded-md mr-4' />
-                <h3 className='text-lg font-semibold'>{label}</h3>
-            </div>
-
-            <LevelModal
-                isOpen={isModalOpen}
-                onClose={() => setModalOpen(false)}
-                selectedLevel={selectedLevel}
-                setSelectedLevel={setSelectedLevel}
-                onStartGame={handleStartGame}
-            />
-        </div>
-    );
-};
 
 export default CategoryCard
