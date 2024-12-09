504b97fc4c61a7f876fdacf2159c57d3d970017d
Mariana Rodrigues Kemmerich <marianarkemmerich@gmail.com>
2024-12-09 10:53:27 -0300

    feat: define rewards array

---
+++ src/data/rewardsData.ts
@@ -0,0 +1,95 @@
+const rewards = [
+    {
+      id: 1,
+      category: 'fauna',
+      level: 'fácil',
+      image: 'image/rewards/zorrilho.png',
+      requiredPoints: 10,
+      commonName: 'Zorrilho',
+      scientificName: 'Panthera onca',
+      description: 'A onça-pintada é o maior felino das Américas e possui um papel importante no equilíbrio ecológico.'
+    },
+    {
+      id: 2,
+      category: 'fauna',
+      level: 'médio',
+      image: 'image/rewards/quero-quero.png',
+      requiredPoints: 20,
+      commonName: 'Quero-quero',
+      scientificName: 'Myrmecophaga tridactyla',
+      description: 'O tamanduá-bandeira utiliza sua longa língua para capturar formigas e cupins, sendo crucial para o ecossistema.'
+    },
+    {
+      id: 3,
+      category: 'fauna',
+      level: 'difícil',
+      image: 'image/rewards/gato-palheiro-pampeano.png',
+      requiredPoints: 30,
+      commonName: 'Gato-palheiro-pampeano',
+      scientificName: 'Anodorhynchus hyacinthinus',
+      description: 'A arara-azul é uma das maiores espécies de araras, famosa por sua plumagem azul vibrante.'
+    },
+    {
+      id: 4,
+      category: 'flora',
+      level: 'fácil',
+      image: 'image/rewards/beija-flor-de-barba-azul.png',
+      requiredPoints: 10,
+      commonName: 'Beija-flor-de-barba-azul',
+      scientificName: 'Handroanthus albus',
+      description: 'O ipê-amarelo é uma árvore símbolo do Brasil, conhecida por suas flores amarelas deslumbrantes.'
+    },
+    {
+      id: 5,
+      category: 'flora',
+      level: 'médio',
+      image: 'image/rewards/graxaim-do-campo.png',
+      requiredPoints: 20,
+      commonName: 'Graxaim-do-campo',
+      scientificName: 'Jacaranda mimosifolia',
+      description: 'O jacarandá encanta com suas flores lilases, muito comuns em paisagismo urbano.'
+    },
+    {
+      id: 6,
+      category: 'flora',
+      level: 'difícil',
+      image: 'image/rewards/tuco-tuco.png',
+      requiredPoints: 30,
+      commonName: 'Tuco-tuco',
+      scientificName: 'Cereus jamacaru',
+      description: 'O mandacaru é uma planta adaptada ao semiárido brasileiro, essencial para a fauna local.'
+    },
+    {
+      id: 7,
+      category: 'geral',
+      level: 'fácil',
+      image: 'image/rewards/gato-do-mato-grande.png',
+      requiredPoints: 10,
+      commonName: 'Gato-do-mato-grande',
+      scientificName: 'Ramphastos toco',
+      description: 'O tucano é conhecido por seu grande bico colorido e sua presença em florestas tropicais.'
+    },
+    {
+      id: 8,
+      category: 'geral',
+      level: 'médio',
+      image: 'image/rewards/papa-moscas-do-campo.png',
+      requiredPoints: 20,
+      commonName: 'Papa-moscas-do-campo',
+      scientificName: 'Chrysocyon brachyurus',
+      description: 'O lobo-guará é um animal icônico do cerrado brasileiro, conhecido por sua pelagem avermelhada.'
+    },
+    {
+      id: 9,
+      category: 'geral',
+      level: 'difícil',
+      image: 'image/rewards/veado-campeiro.png',
+      requiredPoints: 30,
+      commonName: 'Veado-campeiro',
+      scientificName: 'Victoria amazonica',
+      description: 'A vitória-régia é uma planta aquática nativa da Amazônia, famosa por suas folhas gigantes.'
+    }
+  ]
+  
+  export default rewards
+  
