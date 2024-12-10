15dbdbfabaf284105c84661420153b8dda732e81
Mariana Rodrigues Kemmerich <marianarkemmerich@gmail.com>
2024-12-10 14:15:33 -0300

    chore: define rewards data

--- src/data/rewardsData.ts
+++ src/data/rewardsData.ts
@@ -1,95 +1,95 @@
 const rewards = [
-    {
-      id: 1,
-      category: 'fauna',
-      level: 'fácil',
-      image: 'image/rewards/zorrilho.png',
-      requiredPoints: 10,
-      commonName: 'Zorrilho',
-      scientificName: 'Panthera onca',
-      description: 'A onça-pintada é o maior felino das Américas e possui um papel importante no equilíbrio ecológico.'
-    },
-    {
-      id: 2,
-      category: 'fauna',
-      level: 'médio',
-      image: 'image/rewards/quero-quero.png',
-      requiredPoints: 20,
-      commonName: 'Quero-quero',
-      scientificName: 'Myrmecophaga tridactyla',
-      description: 'O tamanduá-bandeira utiliza sua longa língua para capturar formigas e cupins, sendo crucial para o ecossistema.'
-    },
-    {
-      id: 3,
-      category: 'fauna',
-      level: 'difícil',
-      image: 'image/rewards/gato-palheiro-pampeano.png',
-      requiredPoints: 30,
-      commonName: 'Gato-palheiro-pampeano',
-      scientificName: 'Anodorhynchus hyacinthinus',
-      description: 'A arara-azul é uma das maiores espécies de araras, famosa por sua plumagem azul vibrante.'
-    },
-    {
-      id: 4,
-      category: 'flora',
-      level: 'fácil',
-      image: 'image/rewards/beija-flor-de-barba-azul.png',
-      requiredPoints: 10,
-      commonName: 'Beija-flor-de-barba-azul',
-      scientificName: 'Handroanthus albus',
-      description: 'O ipê-amarelo é uma árvore símbolo do Brasil, conhecida por suas flores amarelas deslumbrantes.'
-    },
-    {
-      id: 5,
-      category: 'flora',
-      level: 'médio',
-      image: 'image/rewards/graxaim-do-campo.png',
-      requiredPoints: 20,
-      commonName: 'Graxaim-do-campo',
-      scientificName: 'Jacaranda mimosifolia',
-      description: 'O jacarandá encanta com suas flores lilases, muito comuns em paisagismo urbano.'
-    },
-    {
-      id: 6,
-      category: 'flora',
-      level: 'difícil',
-      image: 'image/rewards/tuco-tuco.png',
-      requiredPoints: 30,
-      commonName: 'Tuco-tuco',
-      scientificName: 'Cereus jamacaru',
-      description: 'O mandacaru é uma planta adaptada ao semiárido brasileiro, essencial para a fauna local.'
-    },
-    {
-      id: 7,
-      category: 'geral',
-      level: 'fácil',
-      image: 'image/rewards/gato-do-mato-grande.png',
-      requiredPoints: 10,
-      commonName: 'Gato-do-mato-grande',
-      scientificName: 'Ramphastos toco',
-      description: 'O tucano é conhecido por seu grande bico colorido e sua presença em florestas tropicais.'
-    },
-    {
-      id: 8,
-      category: 'geral',
-      level: 'médio',
-      image: 'image/rewards/papa-moscas-do-campo.png',
-      requiredPoints: 20,
-      commonName: 'Papa-moscas-do-campo',
-      scientificName: 'Chrysocyon brachyurus',
-      description: 'O lobo-guará é um animal icônico do cerrado brasileiro, conhecido por sua pelagem avermelhada.'
-    },
-    {
-      id: 9,
-      category: 'geral',
-      level: 'difícil',
-      image: 'image/rewards/veado-campeiro.png',
-      requiredPoints: 30,
-      commonName: 'Veado-campeiro',
-      scientificName: 'Victoria amazonica',
-      description: 'A vitória-régia é uma planta aquática nativa da Amazônia, famosa por suas folhas gigantes.'
-    }
-  ]
-  
-  export default rewards
+  {
+    id: 1,
+    category: 'fauna',
+    level: 'fácil',
+    image: 'image/rewards/zorrilho.png',
+    requiredPoints: 75,
+    commonName: 'Zorrilho',
+    scientificName: 'Conepatus chinga',
+    description: 'O zorrilho é um carnívoro de pequeno porte que vive em áreas abertas de campos nativos do bioma Pampa, litoral e Campos de Cima da Serra. Sua alimentação se baseia em insetos, aves, ovos de tartaruga, frutas e vegetais. Este animal possui glândulas de odor que esguicham um líquido quando se sentem ameaçados ou se encontram em situações de perigo.'
+  },
+  {
+    id: 2,
+    category: 'fauna',
+    level: 'médio',
+    image: 'image/rewards/quero-quero.png',
+    requiredPoints: 525,
+    commonName: 'Quero-quero',
+    scientificName: 'Vanellus chilensis',
+    description: 'Quero-quero é uma ave de porte médio com padrão de plumagem nas cores preto, cinza e branco, além de possuir esporões ósseos no ângulo das asas. Esta ave se alimenta de pequenos invertebrados do solo. É conhecida por seu comportamento tipicamente agressivo e vocalização estridente característica, além da territorialidade marcada, principalmente na estação reprodutiva.'
+  },
+  {
+    id: 3,
+    category: 'fauna',
+    level: 'difícil',
+    image: 'image/rewards/gato-palheiro-pampeano.png',
+    requiredPoints: 1050,
+    commonName: 'Gato-palheiro-pampeano',
+    scientificName: 'Leopardus munoai',
+    description: 'Conhecido mundialmente como "Gato dos pampas", o Gato-palheiro-pampeano é uma espécie de felino endêmica do bioma Pampa, ocorrendo na região sul do Rio Grande do Sul, Uruguai e nordeste da Argentina. Possui uma dieta mais generalista que outras espécies de pequenos felinos de ocorrência no Pampa, consumindo principalmente roedores terrestres de pequeno porte. Sua aparência se caracteriza pela pelagem longa e áspera, variando a tonalidade de acizentado à avermelhado. Atualmente, é considerado o felino mais criticamente ameaçados de extinção no Brasil, sendo a descaracterização de seu habitat a maior ameaça à sua existência.'
+  },
+  {
+    id: 4,
+    category: 'flora',
+    level: 'fácil',
+    image: 'image/rewards/beija-flor-de-barba-azul.png',
+    requiredPoints: 175,
+    commonName: 'Beija-flor-de-barba-azul',
+    scientificName: 'Handroanthus albus',
+    description: 'O ipê-amarelo é uma árvore símbolo do Brasil, conhecida por suas flores amarelas deslumbrantes.'
+  },
+  {
+    id: 5,
+    category: 'flora',
+    level: 'médio',
+    image: 'image/rewards/graxaim-do-campo.png',
+    requiredPoints: 525,
+    commonName: 'Graxaim-do-campo',
+    scientificName: 'Jacaranda mimosifolia',
+    description: 'O jacarandá encanta com suas flores lilases, muito comuns em paisagismo urbano.'
+  },
+  {
+    id: 6,
+    category: 'flora',
+    level: 'difícil',
+    image: 'image/rewards/tuco-tuco.png',
+    requiredPoints: 1050,
+    commonName: 'Tuco-tuco',
+    scientificName: 'Cereus jamacaru',
+    description: 'O mandacaru é uma planta adaptada ao semiárido brasileiro, essencial para a fauna local.'
+  },
+  {
+    id: 7,
+    category: 'geral',
+    level: 'fácil',
+    image: 'image/rewards/gato-do-mato-grande.png',
+    requiredPoints: 175,
+    commonName: 'Gato-do-mato-grande',
+    scientificName: 'Ramphastos toco',
+    description: 'O tucano é conhecido por seu grande bico colorido e sua presença em florestas tropicais.'
+  },
+  {
+    id: 8,
+    category: 'geral',
+    level: 'médio',
+    image: 'image/rewards/papa-moscas-do-campo.png',
+    requiredPoints: 525,
+    commonName: 'Papa-moscas-do-campo',
+    scientificName: 'Chrysocyon brachyurus',
+    description: 'O lobo-guará é um animal icônico do cerrado brasileiro, conhecido por sua pelagem avermelhada.'
+  },
+  {
+    id: 9,
+    category: 'geral',
+    level: 'difícil',
+    image: 'image/rewards/veado-campeiro.png',
+    requiredPoints: 1050,
+    commonName: 'Veado-campeiro',
+    scientificName: 'Victoria amazonica',
+    description: 'A vitória-régia é uma planta aquática nativa da Amazônia, famosa por suas folhas gigantes.'
+  }
+]
+
+export default rewards
   
