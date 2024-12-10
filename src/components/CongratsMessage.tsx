4234c1710c2048760f6509342d71ffa8feffdf84
Mariana Rodrigues Kemmerich <marianarkemmerich@gmail.com>
2024-12-10 16:04:51 -0300

    feat: add congratulations message component

---
+++ src/components/CongratsMessage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CongratsMessage = () => {
+  return (
+    <div>
+        <img src='/image/messages/parabens.svg' alt='Parabéns! Você concluiu o quiz.' />
+    </div>
+  )
+}
+
+export default CongratsMessage
