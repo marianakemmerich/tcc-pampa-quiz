72df19aebc72324cd0d5ee27e7167d1812a41532
Mariana Rodrigues Kemmerich <marianarkemmerich@gmail.com>
2024-12-10 16:23:39 -0300

    style: improve components overall appearance

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,11 +1,13 @@
 interface QuestionProps {
-    question: string
+  question: string
 }
 
 const Question = ({ question }: QuestionProps) => {
   return (
-    <div>
-        <h2 className="text-xl font-bold mb-12">{question}</h2>
+    <div className="w-full flex items-center justify-center mt-8 mb-2">
+      <div className="w-2/3 p-4">
+        <h2 className="text-xl text-white font-bold text-center">{question}</h2>
+      </div>    
     </div>
   )
 }
