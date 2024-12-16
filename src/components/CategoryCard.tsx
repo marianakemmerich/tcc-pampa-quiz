4a4d674d6eb3bf32557a30fc50d7827c46067695
Mariana Rodrigues Kemmerich <marianarkemmerich@gmail.com>
2024-12-16 17:23:23 -0300

    style: improve component overall appearance

--- src/components/CategoryCard.tsx
+++ src/components/CategoryCard.tsx
@@ -5,7 +5,7 @@ const CategoryCard = ({ label, category, image, className, onClick }: CategoryCa
   return (
     <div
       onClick={onClick}
-      className={`card ${className} w-[300px] h-[200px] flex items-center p-4 gap-4 rounded-md cursor-pointer`}
+      className={`card ${className} w-[300px] h-[200px] flex items-center p-4 gap-4 shadow-lg rounded-md cursor-pointer`}
     >
       <img src={image} alt={category} className="w-[150px] object-contain" />
       <p className="text-4xl font-bold text-white">{label}</p>
