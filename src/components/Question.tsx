bd0497c5f6032d110310103852c77dd00485af45
Mariana Rodrigues Kemmerich <marianarkemmerich@gmail.com>
2024-12-15 22:30:24 -0300

    style: change font to poppins

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -11,9 +11,9 @@ const Question = ({ question, category }: QuestionProps) => {
   }
 
   return (
-    <div className="w-full flex items-center justify-center mt-8 mb-2">
-      <div className="mx-8 p-4">
-        <h2 className={`text-xl font-bold text-center ${categoryColor[category]}`}>
+    <div className='w-full flex items-center justify-center mt-8 mb-2'>
+      <div className='mx-8 p-4'>
+        <h2 className={`text-xl font-bold font-poppins text-center ${categoryColor[category]}`}>
           {question}
         </h2>
       </div>
