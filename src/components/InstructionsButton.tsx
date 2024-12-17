9334582a40207ccbf7256ec7db427e5150a81645
Mariana Rodrigues Kemmerich <marianarkemmerich@gmail.com>
2024-12-17 16:53:46 -0300

    feat: add instructions button component

---
+++ src/components/InstructionsButton.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import Help from '../assets/icons/help-icon.png'
+
+interface InstructionsButtonProps {
+  onBackClick?: () => void
+  navigateTo?: string
+}
+
+const InstructionsButton = ({ onBackClick, navigateTo }: InstructionsButtonProps) => {
+  const handleBack = () => {
+    if (onBackClick) {
+      onBackClick()
+    } else if (navigateTo) {
+      window.location.href = navigateTo
+    } else {
+      window.history.back()
+    }
+  }
+
+  return (
+    <div className='p-6'>
+      <button
+        onClick={handleBack}
+        className='transition-transform duration-200 ease-in-out hover:scale-110'
+      >
+        <img
+          src={Help}
+          alt='Voltar'
+          className='w-[50px] md:w-[60px] mt-4 ml-4 md:ml-8'
+        />
+      </button>
+    </div>
+  )
+}
+
+export default InstructionsButton
