03e3bf2ffd09967a16e3a2d619a42614f525d1ce
Mariana Rodrigues Kemmerich <marianarkemmerich@gmail.com>
2024-12-19 18:46:39 -0300

    feat: add user button component

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -1,11 +1,24 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import { onAuthStateChanged } from 'firebase/auth'
+import { useNavigate } from 'react-router-dom'
+import { auth } from '../firebase'
 import BackButton from '../components/BackButton'
 import Button from '../components/Button'
 import InstructionsModal from '../components/InstructionsModal'
 import InstructionsButton from '../components/InstructionsButton'
+import UserButton from '../components/UserButton'
 
 const Menu = () => {
   const [isModalOpen, setIsModalOpen] = useState(false)
+  const [isGuest, setIsGuest] = useState(false)
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setIsGuest(user?.isAnonymous || false)
+    })
+    return unsubscribe
+  }, [])
 
   const openModal = () => setIsModalOpen(true)
   const closeModal = () => setIsModalOpen(false)
@@ -19,9 +32,12 @@ const Menu = () => {
         backgroundPosition: 'center',
       }}
     >
-      <header className='fixed top-2 md:top-0 w-screen flex justify-between px-4 items-center'>
+      <header
+        className='fixed top-2 md:top-0 w-screen flex justify-between px-4 items-center'
+      >
         <BackButton navigateTo='/' />
-        <div className='absolute top-2 right-4'>
+        <div className='flex items-center space-x-4 absolute top-2 right-4'>
+          <UserButton isGuest={isGuest} />
           <InstructionsButton onBackClick={openModal} />
         </div>
       </header>
