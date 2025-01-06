6db27c6b2a2ee2424bf602a11108e4379e8dfb46
Mariana Rodrigues Kemmerich <marianarkemmerich@gmail.com>
2025-01-06 03:31:20 -0300

    chore: update data info

--- src/data/rewardsData.ts
+++ src/data/rewardsData.ts
@@ -14,7 +14,7 @@ const rewards = [
     category: 'fauna',
     level: 'médio',
     image: 'image/rewards/quero-quero.png',
-    requiredPoints: 525,
+    requiredPoints: 75,
     commonName: 'Quero-quero',
     scientificName: 'Vanellus chilensis',
     description: 'Quero-quero é uma ave de porte médio com padrão de plumagem nas cores preto, cinza e branco, além de possuir esporões ósseos no ângulo das asas. Esta ave se alimenta de pequenos invertebrados do solo. É conhecida por seu comportamento tipicamente agressivo e vocalização estridente característica, além da territorialidade marcada, principalmente na estação reprodutiva.'
@@ -24,72 +24,71 @@ const rewards = [
     category: 'fauna',
     level: 'difícil',
     image: 'image/rewards/gato-palheiro-pampeano.png',
-    requiredPoints: 1050,
+    requiredPoints: 75,
     commonName: 'Gato-palheiro-pampeano',
     scientificName: 'Leopardus munoai',
-    description: 'Conhecido mundialmente como "Gato dos pampas", o Gato-palheiro-pampeano é uma espécie de felino endêmica do bioma Pampa, ocorrendo na região sul do Rio Grande do Sul, Uruguai e nordeste da Argentina. Possui uma dieta mais generalista que outras espécies de pequenos felinos de ocorrência no Pampa, consumindo principalmente roedores terrestres de pequeno porte. Sua aparência se caracteriza pela pelagem longa e áspera, variando a tonalidade de acizentado à avermelhado. Atualmente, é considerado o felino mais criticamente ameaçados de extinção no Brasil, sendo a descaracterização de seu habitat a maior ameaça à sua existência.'
+    description: 'Espécie de felino endêmica do bioma Pampa, ocorrendo no Rio Grande do Sul, Uruguai e Argentina. Possui uma dieta mais generalista que outras espécies de pequenos felinos, consumindo principalmente pequenos roedores. Sua aparência se caracteriza pela pelagem longa e áspera, variando a tonalidade de acizentado à avermelhado. Atualmente, é considerado o felino mais criticamente ameaçado de extinção no Brasil, sendo a descaracterização de seu habitat a maior ameaça à sua existência.'
   },
   {
     id: 4,
     category: 'flora',
     level: 'fácil',
-    image: 'image/rewards/beija-flor-de-barba-azul.png',
-    requiredPoints: 175,
-    commonName: 'Beija-flor-de-veste-preta',
-    scientificName: 'Anthracothorax nigricollis',
-    description: 'O ipê-amarelo é uma árvore símbolo do Brasil, conhecida por suas flores amarelas deslumbrantes.'
+    image: 'image/rewards/bugio-preto.png',
+    requiredPoints: 75,
+    commonName: 'Bugio-preto',
+    scientificName: 'Alouatta caraya',
+    description: 'Este primata é encontrado nas matas ciliares do bioma Pampa, assim como em matas de araucárias e na Mata Atlântica. Emite uma vocalização característica que pode alcançar longas distâncias como marcação de território e organização do grupo. Sua alimentação baseia-se em folhas, sementes, frutos e flores.'
   },
   {
     id: 5,
     category: 'flora',
     level: 'médio',
     image: 'image/rewards/graxaim-do-campo.png',
-    requiredPoints: 525,
+    requiredPoints: 75,
     commonName: 'Graxaim-do-campo',
-    scientificName: 'Cerdocyon thous',
-    description: 'O jacarandá encanta com suas flores lilases, muito comuns em paisagismo urbano.'
+    scientificName: 'Lycalopex gymnocercus',
+    description: 'Também conhecido como raposa-dos-pampas, este animal é encontrado no bioma Pampa e Campos de Cima da Serra. Possui coloração cinza-amarelada, patas longas e finas e orelhas grandes. O graxaim-do-campo é onívoro, alimentando-se principalmente de pequenos vertebrados, invertebrados e frutas.'
   },
   {
     id: 6,
     category: 'flora',
     level: 'difícil',
     image: 'image/rewards/tuco-tuco.png',
-    requiredPoints: 1050,
+    requiredPoints: 75,
     commonName: 'Tuco-tuco',
-    scientificName: 'Ctenomys torquatus',
-    description: 'O mandacaru é uma planta adaptada ao semiárido brasileiro, essencial para a fauna local.'
+    scientificName: 'Ctenomys ibicuiensis',
+    description: 'Ocorrendo em uma região próxima ao município de Manoel Viana, o tuco-tuco é uma espécie de roedor endêmica do bioma Pampa, sendo descrita somente em 2012. A população deste animal se severamente ameaçada por atividades agrícolas, silviculturas, monoculturas e expansão da desertificação na região.'
   },
   {
     id: 7,
     category: 'geral',
     level: 'fácil',
     image: 'image/rewards/gato-do-mato-grande.png',
-    requiredPoints: 175,
+    requiredPoints: 75,
     commonName: 'Gato-do-mato-grande',
     scientificName: 'Leopardus geoffroyi',
-    description: 'O tucano é conhecido por seu grande bico colorido e sua presença em florestas tropicais.'
+    description: 'Este felino possui tamanho similar ao do gato doméstico. Apresenta coloração amarelo-acizentada com pintas pretas sólidas. Também existem indivíduos de coloração preta (melânicos). Alimentam-se de pequenos mamíferos, aves, lagartos e anfíbios.'
   },
   {
     id: 8,
     category: 'geral',
     level: 'médio',
-    image: 'image/rewards/papa-moscas-do-campo.png',
-    requiredPoints: 525,
-    commonName: 'Caminheiro-de-unha-curta',
-    scientificName: 'Anthus furcatus',
-    description: 'O lobo-guará é um animal icônico do cerrado brasileiro, conhecido por sua pelagem avermelhada.'
+    image: 'image/rewards/lagartixa-das-dunas.png',
+    requiredPoints: 75,
+    commonName: 'Lagartixa-das-dunas',
+    scientificName: 'Liolaemus arambarensis',
+    description: 'Endêmico do Rio Grande do Sul, este pequeno lagarto é encontrado em restingas arenosas do bioma Pampa.  Possui uma coloração críptica, semelhante ao substrato arenoso, o que lhe confere uma excelente camuflagem. Quando filhotes, alimentam-se unicamente de artrópodes, enquanto adultos alimentam-se de artrópodes e material vegetal.'
   },
   {
     id: 9,
     category: 'geral',
     level: 'difícil',
     image: 'image/rewards/veado-campeiro.png',
-    requiredPoints: 1050,
+    requiredPoints: 75,
     commonName: 'Veado-campeiro',
     scientificName: 'Ozotoceros bezoarticus',
-    description: 'A vitória-régia é uma planta aquática nativa da Amazônia, famosa por suas folhas gigantes.'
+    description: 'Também conhecido por Veado-branco, estes animais são encontrados nos campos do bioma Pampa e Campos de Cima da Serra. Possuem pelagem amarronzada, orelhas curtas e pontiagudas e chifres ramificados, que caem anualmente. São considerados pastadores-podadores que se alimentam  de folhas jovens, flores e sementes.'
   }
 ]
 
 export default rewards
-  
